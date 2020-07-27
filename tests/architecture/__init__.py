f70c952ef6619d2f680183bfbbcff957eb9585b8
tylerik1 <erikkniaz@hotmail.com>
2020-07-27 10:37:48 -0400

    Update __init__.py

--- tests/architecture/__init__.py
+++ tests/architecture/__init__.py
@@ -5,5 +5,7 @@ Created on Apr 1, 2019
 '''
 import os
 import pkgutil
+from tests.architecture.add_to_cart import *
+from tests.architecture.credit_card import *
 
 __all__ = list(module for _, module, _ in pkgutil.iter_modules([os.path.dirname(__file__)]))
