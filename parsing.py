c3e9697e0189ec5bb1f70f5c7539f4dff47531ad
tylerik1 <erikkniaz@hotmail.com>
2021-09-09 14:14:14 -0400

    Update parsing.py
    Added a function to get all the functions in the testing files.  This assists the bulk_parallel_process function.

--- parsing.py
+++ parsing.py
@@ -26,7 +26,7 @@ def tag(*args, **kwargs):
 
 
 
-def group_tags():
+def group_tags(logger):
     '''
     Groups all decorators of the same tag name into test sets.
     '''
@@ -47,7 +47,8 @@ def group_tags():
                     path_list.append(entry.path)
                 elif entry.is_dir() and sub_folders:   # if its a directory, then repeat process as a nested function
                     path_list = find_files_in_folder(entry.path, path_list, extension, sub_folders)
-        except OSError:
+        except OSError as e:
+            print(e)
             print('Cannot access ' + path +'. Probably a permissions error')
 
         return path_list
@@ -61,16 +62,17 @@ def group_tags():
         #save the path to all the python files
         path_list = []
         path_list = find_files_in_folder(dir_name, path_list, extension, True)
-        path_list.remove(os.path.join(current, "decorators.py"))
-        path_list.remove(os.path.join(current, "test_set_runner.py"))
-        path_list.remove(os.path.join(current, "parsing.py"))
-    except:
-        dir_name = r'pypyke'
-        extension = ".py"
+        if os.path.join(current, "test_set_runner.py") in path_list:
+            path_list.remove(os.path.join(current, "test_set_runner.py"))
+        if os.path.join(current, "parsing.py") in path_list:
+            path_list.remove(os.path.join(current, "parsing.py"))
 
-        #save the path to all the python files
-        path_list = []
-        path_list = find_files_in_folder(dir_name, path_list, extension, True)
+
+    except Exception as e:
+        logger.info("hit an exception while trying to save path to python files")
+        logger.info(e)
+        pass
+
 
 
     #parse each file and save each function and its decorators
@@ -78,7 +80,7 @@ def group_tags():
     tags = []
     for py_file in path_list:
         count = 0
-        lines = open(py_file).readlines()
+        lines = open(py_file, encoding="utf8").readlines()
         for line in lines:
             count += 1
             #if tag is commented out, skip it
@@ -96,16 +98,21 @@ def group_tags():
                     save_line = index
                     if "def" in lines[save_line]:
                         #func_names.append((str, (ntpath.basename(py_file)[:-3], lines[save_line].replace("def ", "").replace(":\n", ""))))
-                        func_names.append((str, (py_file.split('architecture')[1].replace('\\', ".").strip('.')[:-3], lines[save_line].replace("def ", "").replace(":\n", ""))))
+                        if 'architecture' in py_file:
+                            func_names.append((str, (py_file.split('architecture')[1].replace('\\', ".").strip('.')[:-3], lines[save_line].replace("def ", "").replace(":\n", ""))))
+                        if 'infrastructure' in py_file:
+                            func_names.append((str, (py_file.split('infrastructure')[1].replace('\\', ".").strip('.')[:-3], lines[save_line].replace("def ", "").replace(":\n", ""))))
 
     tag_set = list(set(tags))
     func_names = list(set(func_names))
+
 
 
     test_list = {}
 
     for i, tag in enumerate(tag_set):
         test_list[tag] = i
+
 
     for tag in tag_set:
         temp_list = []
@@ -114,4 +121,98 @@ def group_tags():
                 temp_list.append(function[1])
         test_list[tag] = temp_list
 
+
+    #All tests that are marked with a tag will be added to the code coverage suite.
+    tmp_code_coverage_list = []
+    for function in func_names:
+        #exclude tests marked bulk_parallel_process from code coverage suite
+        if function[0] == 'bulk_parallel_process':
+            continue
+        tmp_code_coverage_list.append(function[1])
+    test_list['code_coverage'] = set(tmp_code_coverage_list)
+
     return test_list
+
+
+
+
+
+
+
+
+
+def get_functions(logger, function):
+    '''
+    Returns the file that which the function belongs to.  Does not return functions in helper files.
+    '''
+
+    def find_files_in_folder(path, path_list, extension, sub_folders=True):
+        '''
+        Recursive function to find all files of an extension type in a folder (and optionally in all subfolders too)
+
+        path:        Base directory to find files
+        pathList:    A list that stores all paths
+        extension:   File extension to find
+        subFolders:  Bool.  If True, find files in all subfolders under path. If False, only searches files in the specified folder
+        '''
+
+        try:   # Trapping a OSError:  File permissions problem I believe
+            for entry in os.scandir(path):
+                if entry.is_file() and entry.path.endswith(extension):
+                    path_list.append(entry.path)
+                elif entry.is_dir() and sub_folders:   # if its a directory, then repeat process as a nested function
+                    path_list = find_files_in_folder(entry.path, path_list, extension, sub_folders)
+        except OSError as e:
+            print(e)
+            print('Cannot access ' + path +'. Probably a permissions error')
+
+        return path_list
+
+
+    try:
+        current = os.getcwd()
+        dir_name = os.path.abspath(current)
+        extension = ".py"
+
+        #save the path to all the python files
+        path_list = []
+        path_list = find_files_in_folder(dir_name, path_list, extension, True)
+        if os.path.join(current, "test_set_runner.py") in path_list:
+            path_list.remove(os.path.join(current, "test_set_runner.py"))
+        if os.path.join(current, "parsing.py") in path_list:
+            path_list.remove(os.path.join(current, "parsing.py"))
+
+
+    except Exception as e:
+        logger.info("hit an exception while trying to save path to python files")
+        logger.info(e)
+        pass
+
+    func_names = []
+    for py_file in path_list:
+        lines = open(py_file, encoding="utf8").readlines()
+        for line in lines:
+            if 'def ' in line:
+                if 'architecture' in py_file:
+                    func_names.append(((py_file.split('architecture')[1].replace('\\', ".").strip('.')[:-3], line.replace("def ", "").replace(":\n", ""))))
+                if 'infrastructure' in py_file:
+                    func_names.append(
+                        ((py_file.split('infrastructure')[1].replace('\\', ".").strip('.')[:-3], line.replace("def ", "").replace(":\n", ""))))
+
+
+    for func in func_names:
+        if function+'(' in func[1]:
+            if '_helpers' in func[0]:
+                continue
+            return func[0].split('.')[1] + '.' + function
+
+
+
+    logger.info('Unable to locate file for function: ' + function)
+    logger.info('Check to make sure the function is spelled correctly.')
+
+    return False
+
+
+
+
