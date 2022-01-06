481a1e9d03ffeb1ecbb1156ec3a0ea935cb15897
tylerik1 <erikkniaz@hotmail.com>
2022-01-06 16:29:46 -0500

    Update first_time_setup.py

--- first_time_setup.py
+++ first_time_setup.py
@@ -2,11 +2,13 @@ import os
 
 existing_python_path = os.environ.get("PYTHONPATH")
 
-if os.getcwd() in existing_python_path:
-  return
 
+#if the path does not exist, create the python path and add the framework
 if not existing_python_path:
   os.environ['PYTHONPATH'] = os.getcwd()
-  
+
+#if the path exists, append the framework to the end.  If the framework is already there, return
 else:
+  if os.getcwd() in existing_python_path:
+    return
   os.environ['PYTHONPATH'] = existing_python_path + ";" + os.getcwd()
